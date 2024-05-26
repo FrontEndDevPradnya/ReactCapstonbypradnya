b727b28fd92013bf1c28a60708513e4b62454da6
Pensalwar <2078291@cognizant.com>
2024-05-26 10:36:57 +0530

    added logo and og

---
+++ littlelemon/src/Nav.js
@@ -0,0 +1,7 @@
+return <nav>
+    <img src="ReactCapstonbypradnya\littlelemon\src\assets\Applying-design-fundmantals-in-Figma---Assets\icons_assets\Logo.svg"></img>
+    <meta name="description" content="little lemon restaurant"/>
+    <meta name="og:title" content="Little lemon"/>
+    <meta name="og:description" content="this is food outlet"/>
+    <meta name="og:image" content="image"/>
+</nav>
